9f56ec49b69636b1a9f776f9a625f9c13428e408
Vladizslav <gon.vlad02@gmail.com>
2019-11-02 18:20:17 +0300

    correct zero-decimal part mistake

--- fhex.h
+++ fhex.h
@@ -1,70 +1,32 @@
 // Convert fractional part of float number to hexadecimal
-
 #include <math.h>
 
 int fhex(double fpnum) {
     double fracp;
     int decp;
 
-    fracp  = fmod(fpnum, (int) fpnum);
+    if (((int) fracp) != 0)
+        fracp  = fmod(fpnum, (int) fpnum);
+    else
+        fracp = fpnum;
     fracp *= 16;
     decp   = (int) fracp;
     decp  *= 16;
 
     for (int i = 0; i < 6; ++i) {
-        fracp  = fmod(fracp, (int) fracp);
+        if (((int) fracp) != 0) 
+            fracp  = fmod(fracp, (int) fracp);
+        else
+            fracp = fracp;
         fracp *= 16;
         decp  += (int) fracp;
         decp  *= 16;
     }
 
-    fracp  = fmod(fracp, (int) fracp);
-    fracp *= 16;
-    decp  += (int) fracp;
-
-    return decp;
-}
-
-int fhex_hardcode(double fpnum) {
-    double fracp;
-    int decp;
-
-    fracp  = fmod(fpnum, (int) fpnum);
-    fracp *= 16;
-    decp   = (int) fracp;
-    decp  *= 16;
-
-    fracp  = fmod(fracp, (int) fracp);
-    fracp *= 16;
-    decp  += (int) fracp;
-    decp  *= 16;
-
-    fracp  = fmod(fracp, (int) fracp);
-    fracp *= 16;
-    decp  += (int) fracp;
-    decp  *= 16;
-
-    fracp  = fmod(fracp, (int) fracp);
-    fracp *= 16;
-    decp  += (int) fracp;
-    decp  *= 16;
-
-    fracp  = fmod(fracp, (int) fracp);
-    fracp *= 16;
-    decp  += (int) fracp;
-    decp  *= 16;
-
-    fracp  = fmod(fracp, (int) fracp);
-    fracp *= 16;
-    decp  += (int) fracp;
-    decp  *= 16;
-
-    fracp  = fmod(fracp, (int) fracp);
-    fracp *= 16;
-    decp  += (int) fracp;
-    decp  *= 16;
-
-    fracp  = fmod(fracp, (int) fracp);
+    if (((int) fracp) != 0)
+        fracp  = fmod(fracp, (int) fracp);
+    else
+        fracp = fracp;
     fracp *= 16;
     decp  += (int) fracp;
 
